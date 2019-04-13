6448a6012b623b3041fc943edb8a9511360a34c8
ses880 <ses880@nyu.edu>
2019-04-13 15:58:42 -0400

    commit

---
+++ Grid/Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    TextMeshPro movesText;
+    public int moves = 6;
+    public bool gameOver;
+    public static PlayerMoveScript Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moves = 6;
+        movesText = GetComponent<TextMeshPro>();
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movesText.text = moves.ToString();
+
+        /*if (Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d") || Input.GetKeyDown("w"))
+        {
+            moves -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moves -= 1;
+        }
+        */
+        if (moves < 0)
+        {
+            moves = 0;
+        }
+
+        if (moves == 0)
+        {
+            gameOver = true;
+        }
+
+    }
+}
---
+++ Grid/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+
+    Transform player;
+    float playerX;
+    float playerY;
+    //Vector2 lastPosition;
+    public Vector2 PlayerPos;
+
+    //numbers
+    /*public Text movesText;
+    public int moves = 6;
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        PlayerPos = new Vector2(GridMaker.Width / 2, GridMaker.Height / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //movesText.text = "" + moves; 
+
+
+        //Vector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //moving with keys
+        if (Input.GetKeyDown("d") || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            if (PlayerPos.x - 1 >= 0)
+            {
+                movePlayer(-1, 0);
+                PlayerMoveScript.Instance.moves--;
+
+            }
+        }
+
+        if (Input.GetKeyDown("a") || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+
+            if (PlayerPos.x + 1 <= GridMaker.Width - 1)
+            {
+                movePlayer(1, 0);
+                PlayerMoveScript.Instance.moves--;
+            }
+        }
+       
+        if (Input.GetKeyDown("w") || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+    
+            if (PlayerPos.y - 1 >= 0)
+            {
+                movePlayer(0, -1);
+                Debug.Log("movedup");
+                PlayerMoveScript.Instance.moves--;
+            }
+        }
+
+        if (Input.GetKeyDown("s") || (Input.GetKeyDown(KeyCode.DownArrow)))
+        {
+            if (PlayerPos.y + 1 <= GridMaker.Height - 1)
+            {
+                movePlayer(0, 1);
+                PlayerMoveScript.Instance.moves--;
+            }
+        }
+       
+    }
+    void movePlayer(int x, int y)
+    {
+        Vector2 oldLoc = new Vector2(PlayerPos.x, PlayerPos.y);
+        Vector2 newLoc = new Vector2(PlayerPos.x + x, PlayerPos.y + y);
+
+        GameObject swapTile = GridMaker.tiles[(int) newLoc.x,(int) newLoc.y];
+
+        Vector3 swapPos = swapTile.transform.localPosition;
+
+        swapTile.transform.localPosition = transform.localPosition;
+
+        transform.localPosition = swapPos;
+
+        GridMaker.tiles[(int)oldLoc.x, (int)oldLoc.y] = swapTile;
+
+        GridMaker.tiles[(int)newLoc.x, (int)newLoc.y] = gameObject;
+
+        PlayerPos = newLoc;
+        
+        //moves--;
+        Debug.Log("moved");
+
+    }
+}
